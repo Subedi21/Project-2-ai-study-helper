68c46f02fe951bb01e049d0519eadbb28ac2d7e6
subedi21 <subedilochana@gmail.com>
2026-05-11 17:07:54 -0400

    Fixed Render port

--- server.js
+++ server.js
@@ -155,7 +155,7 @@ app.post("/ask-ai", async (req, res) => {
 });
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
